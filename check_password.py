ed6a271bb6e4e3de93745733ca1e06d3385abc2a
BAIBASH1 <124410504+BAIBASH1@users.noreply.github.com>
2023-04-03 14:49:18 +0300

    Add if __name__="__main__"

--- check_password.py
+++ check_password.py
@@ -4,7 +4,6 @@ import urwid
 def has_digits(password):
     return any(element.isdigit() for element in password)
 
-
 def has_letters(password):
     return any(element.isalpha() for element in password)
 
@@ -27,14 +26,15 @@ def is_very_long(password):
 
 
 def score_count(edit, new_edit_text):
-    score = 0
-    for check in checks:
-        if check(new_edit_text):
-            score += 2
-    reply.set_text(f"Рейтинг вашего пароля: {score}")
+  score = 0
+  for check in checks:
+    if check(new_edit_text):
+        score += 2
+  reply.set_text(f"Рейтинг вашего пароля: {score}")
 
-
-if __name__== '__main__':
+def main():
+    global checks, reply
+    
     checks = [
         has_digits,
         has_upper_letters,
@@ -43,9 +43,17 @@ if __name__== '__main__':
         is_very_long,
         has_lower_letters
     ]
+    
     ask = urwid.Edit('Введите пароль: ', mask='*')
+    
+    
+    
     reply = urwid.Text("")
     menu = urwid.Pile([ask, reply])
     menu = urwid.Filler(menu, valign='top')
-    urwid.connect_signal(ask, 'change', score_count)
+    urwid.connect_signal(ask , 'change', score_count)
     urwid.MainLoop(menu).run()
+
+
+if __name__=="__main__":
+  main()
